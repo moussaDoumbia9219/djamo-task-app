7d0489dbce990f4fc60be3b9547633e17ce20373
moussa doumbia <moussa.doumbia9219@gmail.com>
2021-05-30 15:15:47 +0000

    add dependencies and config typeorm+swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.APP_PORT || 3000
+  app.setGlobalPrefix('api')
+
+  const options = new DocumentBuilder()
+    .setTitle('Djamo Tasks API')  
+    .setDescription('Simple api made for djamo technical tests')
+    .setVersion('1.0')
+    .addTag('djamoTasks')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+  
   await app.listen(3000);
 }
 bootstrap();
