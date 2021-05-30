06ec8b92ba4229489d772d5d76203b80134f049d
moussa doumbia <moussa.doumbia9219@gmail.com>
2021-05-30 20:41:23 +0000

    change db configs

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -2,14 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host:
-    process.env.POSTGRES_HOST || 'ec2-23-23-128-222.compute-1.amazonaws.com',
+  host: process.env.POSTGRES_HOST || 'batyr.db.elephantsql.com',
   port: +process.env.POSTGRES_PORT || 5432,
-  username: process.env.DB_USERNAME || 'opzchgufhbrphp',
-  password:
-    process.env.DB_PASSWORD ||
-    '842579378d06638ff42bffafd069ab22fe5aa233be4d53cbc74dc089570a8ba0',
-  database: process.env.POSTGRES_DB || 'df71n0m8v06anb',
+  username: process.env.DB_USERNAME || 'cqzcfpvd',
+  password: process.env.DB_PASSWORD || 'kcM85y95gx4SWNi_aOjptNUobQLbbuOS',
+  database: process.env.POSTGRES_DB || 'cqzcfpvd',
   entities: [__dirname + '/**/*.entity.ts', __dirname + '/**/*.entity.js'],
   migrationsRun: false,
   logging: true,
