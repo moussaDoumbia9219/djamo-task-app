8c5592b39150a47c0821525da85440ba1b5df276
moussa doumbia <moussa.doumbia9219@gmail.com>
2021-05-30 19:38:57 +0000

    remove local params of db

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,15 @@ import { MassiveModule } from '@nestjsplus/massive';
 @Module({
   imports: [
     MassiveModule.register({
-      user: process.env.POSTGRES_USERNAME || 'postgres',
-      password: process.env.POSTGRES_PASSWORD || 'postgres',
-      host: process.env.POSTGRES_HOST || 'localhost',
+      user: process.env.POSTGRES_USERNAME || 'opzchgufhbrphp',
+      password:
+        process.env.POSTGRES_PASSWORD ||
+        '842579378d06638ff42bffafd069ab22fe5aa233be4d53cbc74dc089570a8ba0',
+      host:
+        process.env.POSTGRES_HOST ||
+        'ec2-23-23-128-222.compute-1.amazonaws.com',
       port: 5432,
-      database: process.env.POSTGRES_DATABASE || 'djamotodo',
+      database: process.env.POSTGRES_DATABASE || 'df71n0m8v06anb',
     }),
     TypeOrmModule.forRoot(typeOrmConfig),
     TodoModule,
