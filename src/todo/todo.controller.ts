7437b65d2e932948f5928b65d1c6c71e41c9f2ad
moussa doumbia <moussa.doumbia9219@gmail.com>
2021-05-30 16:24:57 +0000

    make api controller

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { TodoDto } from './dto/todo.dto';
+import { Todo } from './entity/todo.entity';
+import { TodoService } from './services/todo.service';
+
+@ApiTags('Todo')
+@Controller('todo')
+export class TodoController {
+  constructor(private todoService: TodoService) {}
+
+  @Get()
+  getAllTodo(): Promise<Todo[]> {
+    return this.todoService.getAllTodo();
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTodo(@Body() todoDto: TodoDto): Promise<Todo> {
+    return this.todoService.createTodo(todoDto);
+  }
+
+  @Get('/:id')
+  getTodoById(@Param('id', ParseIntPipe) id: number): Promise<Todo> {
+    return this.todoService.getTodoById(id);
+  }
+
+  @Patch('/:id')
+  updateTodoById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() todoDto: TodoDto,
+  ): Promise<Todo> {
+    return this.todoService.updateTodoById(id, todoDto);
+  }
+
+  @Delete('/:id')
+  deleteTodoById(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return this.todoService.deleteTodoById(id);
+  }
+}
