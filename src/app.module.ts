44c18450acd1c6fb4cde663410a293d62284c0cf
moussa doumbia <moussa.doumbia9219@gmail.com>
2021-05-30 20:02:16 +0000

    add ssl conf to typeorm options

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,8 @@ import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import * as typeOrmConfig from './typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MassiveModule } from '@nestjsplus/massive';
 
 @Module({
-  imports: [
-    MassiveModule.register({
-      user: process.env.POSTGRES_USERNAME || 'opzchgufhbrphp',
-      password:
-        process.env.POSTGRES_PASSWORD ||
-        '842579378d06638ff42bffafd069ab22fe5aa233be4d53cbc74dc089570a8ba0',
-      host:
-        process.env.POSTGRES_HOST ||
-        'ec2-23-23-128-222.compute-1.amazonaws.com',
-      port: 5432,
-      database: process.env.POSTGRES_DATABASE || 'df71n0m8v06anb',
-      ssl: true,
-    }),
-    TypeOrmModule.forRoot(typeOrmConfig),
-    TodoModule,
-  ],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), TodoModule],
 })
 export class AppModule {}
