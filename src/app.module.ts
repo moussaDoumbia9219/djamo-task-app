4f31a097e4f661c99408f962dc4de3dcb00da0db
moussa doumbia <moussa.doumbia9219@gmail.com>
2021-05-30 19:18:01 +0000

    enable heroku db configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,19 @@ import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
 import * as typeOrmConfig from './typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MassiveModule } from '@nestjsplus/massive';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TodoModule],
+  imports: [
+    MassiveModule.register({
+      user: process.env.POSTGRES_USERNAME || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: 5432,
+      database: process.env.POSTGRES_DATABASE || 'djamotodo',
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TodoModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.APP_PORT || 3000;
 
   const options = new DocumentBuilder()
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
